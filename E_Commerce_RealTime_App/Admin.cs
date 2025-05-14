2d3a05eed88f46bb03b0ea3b00be068db8189eb5
sabarirajp <sabarirajpazhani@gmail.com>
2025-05-14 15:21:06 +0530

    Add project files.

---
+++ E_Commerce_RealTime_App/Admin.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce_RealTime_App
+{
+    class Product
+    {
+
+    }
+    internal class Admin
+    {
+        public void AdminPanel()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("------------------------------------------- ** Admin Panel ** -----------------------------------------");
+            Console.ResetColor();
+            Console.WriteLine("                                            1. Add the Products                                        ");
+            Console.WriteLine("                                            2. Update the Product                                      ");
+            Console.WriteLine("                                            3. Delete the Product                                      ");
+            Console.WriteLine("                                            4. View All the Products                                   ");
+            Console.WriteLine("                                            5. View All the Orders                                     ");
+            Console.WriteLine("                                            6. Exit                                                    ");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            int Choice = 0;
+            Choice:
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Enter the Choice : ");
+                Console.ResetColor();
+
+                int choice = int.Parse(Console.ReadLine());
+
+                if (choice == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid choice! Choice must not be Zero");
+                    Console.ResetColor();
+                    goto Choice;
+                }
+                if (choice > 6)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid choice! Choice must be between 1 and 4");
+                    Console.ResetColor();
+                    goto Choice;
+                }
+
+                Choice = choice;
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid choice! Input must not contain characters, symbols, or whitespace");
+                Console.ResetColor();
+                goto Choice;
+            }
+            catch (OverflowException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid choice! Please Enter the Valid Choice");
+                Console.ResetColor();
+                goto Choice;
+            }
+
+           
+        }
+    }
+}
---
+++ E_Commerce_RealTime_App/E_CommerceInterface.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Commerce_RealTime_App;
+
+namespace E_Commerce_Console_App_RealTime
+{
+    internal interface E_CommerceInterface
+    {
+        void AdminLogin();
+    }
+
+    public class E_CommerceMethods : E_CommerceInterface
+    {
+        public void AdminLogin()
+        {
+            string AdminFilePath = @"D:\FileHandling\E_Commerce\Admin.txt";
+
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("                                 --------->> Welcome Admin <<---------                                 ");
+            Console.ResetColor();
+            Console.WriteLine();
+            AdminUserName:
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Enter the User Admin Name : ");
+            Console.ResetColor();
+            string AdminUserName = Console.ReadLine();
+
+
+            if (File.Exists(AdminFilePath))
+            {
+                foreach (string i in File.ReadAllLines(AdminFilePath))
+                {
+                    string[] admin = i.Split(',');
+                    if (admin[0] == AdminUserName)
+                    {
+                        Password:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Enter Admin Password : ");
+                        Console.ResetColor();
+
+                        string password = Console.ReadLine();
+
+                        if (admin[1] == password)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("Authorized :)");
+                            Console.ResetColor();
+
+                            Admin _admin = new Admin();
+                            _admin.AdminPanel();
+
+
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Invalid Password! Please Enter the Valid Password");
+                            Console.ResetColor();
+                            goto Password;
+                        }
+                       
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid Admin User Name. Enter the Correct Admin User Name ");
+                        Console.ResetColor();
+                        goto AdminUserName;
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The admin database is disconnected.Please verify the path and reconnect");
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+        }
+    }
+}
